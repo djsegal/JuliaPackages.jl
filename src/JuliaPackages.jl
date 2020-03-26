072eec95dc7b40ca3bf5fc83b010e88240920866
Dan Segal <dansegal2@gmail.com>
2020-03-25 17:57:02 -0700

    Add list of package dependencies

--- src/JuliaPackages.jl
+++ src/JuliaPackages.jl
@@ -1,5 +1,21 @@
 module JuliaPackages
 
-greet() = print("Hello World!")
+  using Pkg
+  using Revise
+  using ProgressMeter
+
+  using StatsBase
+  using DataFrames
+  using DataStructures
+
+  using CSV
+  using HTTP
+  using JSON
+
+  using Gumbo
+  using Languages
+  using TextAnalysis
+  using AbstractTrees
+  using Dates
 
 end # module
