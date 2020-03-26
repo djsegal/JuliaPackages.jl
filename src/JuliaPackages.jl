8ddc34a4e84dd8c44846c1dd1000887a27239cbe
Dan Segal <dansegal2@gmail.com>
2020-03-26 02:01:12 -0700

    Make it so packages.csv is always recreated

--- src/JuliaPackages.jl
+++ src/JuliaPackages.jl
@@ -42,23 +42,20 @@ module JuliaPackages
   decibans_db = robust_relabel(decibans_db)
 
   if isfile("../data/packages.csv")
-    packages_db = coalesce.(
-      DataFrame(CSV.read("../data/packages.csv")), ""
-    )
-
     @assert isfile("../data/paths.csv")
     paths_db = CSV.read("../data/paths.csv")
     good_paths = collect(zip(paths_db.package, paths_db.path))
+
+    packages_db = _hit_repo_finish(good_paths)
   else
     good_paths, packages_db = hit_repo_api(general_db, decibans_db)
-
-    CSV.write("../data/packages.csv", packages_db)
 
     paths_db = DataFrame(good_paths)
     rename!(paths_db, Symbol.(["package", "path"]))
     CSV.write("../data/paths.csv", DataFrame(paths_db))
   end
 
+  CSV.write("../data/packages.csv", packages_db)
   hit_readme_api(good_paths)
 
 end
