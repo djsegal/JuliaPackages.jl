a548df55d67bbfecd9e84310dab636b90091d998
Dan Segal <dansegal2@gmail.com>
2020-03-25 18:01:08 -0700

    Update main module to load & make all needed calls

--- src/JuliaPackages.jl
+++ src/JuliaPackages.jl
@@ -18,4 +18,47 @@ module JuliaPackages
   using AbstractTrees
   using Dates
 
-end # module
+  export general_db, decibans_db, packages_db
+
+  include("scrape_general_packages.jl")
+  include("scrape_decibans_packages.jl")
+
+  include("package_paths.jl")
+  include("robust_relabel.jl")
+
+  include("combine_datasets.jl")
+  include("custom_get_database.jl")
+
+  include("custom_make_url.jl")
+  include("custom_text.jl")
+
+  include("hit_repo_api.jl")
+  include("hit_readme_api.jl")
+
+  general_db = custom_get_database("general")
+  decibans_db = custom_get_database("decibans")
+
+  general_db, decibans_db = combine_datasets(general_db, decibans_db)
+  decibans_db = robust_relabel(decibans_db)
+
+  if isfile("../data/packages.csv")
+    packages_db = coalesce.(
+      DataFrame(CSV.read("../data/packages.csv")), ""
+    )
+
+    @assert isfile("../data/paths.csv")
+    paths_db = CSV.read("../data/paths.csv")
+    good_paths = collect(zip(paths_db.package, paths_db.path))
+  else
+    good_paths, packages_db = hit_repo_api(general_db, decibans_db)
+
+    CSV.write("../data/packages.csv", packages_db)
+
+    paths_db = DataFrame(good_paths)
+    rename!(paths_db, Symbol.(["package", "path"]))
+    CSV.write("../data/paths.csv", DataFrame(paths_db))
+  end
+
+  hit_readme_api(good_paths)
+
+end
