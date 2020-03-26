029e1fc1beb14cee6c6125fedc6f9223a7bf97ed
Dan Segal <dansegal2@gmail.com>
2020-03-25 17:59:08 -0700

    Add function for saving/loading/scraping databases

---
+++ src/custom_get_database.jl
@@ -0,0 +1,14 @@
+function custom_get_database(cur_name)
+  cur_file = "../data/$(cur_name).csv"
+
+  if isfile(cur_file)
+    cur_db = coalesce.(
+      DataFrame(CSV.read(cur_file)), ""
+    )
+  else
+    cur_db = getfield(JuliaPackages, Symbol("scrape_$(cur_name)_packages"))()
+    CSV.write(cur_file, cur_db)
+  end
+
+  return cur_db
+end
