c9fac13bb65e8213c58a352c93c24aabf0dea7c9
Dan Segal <dansegal2@gmail.com>
2020-04-14 00:21:05 -0700

    Add sorting to package path list

--- src/package_paths.jl
+++ src/package_paths.jl
@@ -4,7 +4,10 @@ function package_paths(general_db, decibans_db)
         collect(zip(general_db.package, lowercase.(join.(zip(general_db.owner, join.(vcat.(general_db.package, ".jl"))), "/"))))
     )
 
-    cur_paths = unique(cur_paths)
+    cur_paths = sort(
+      unique(cur_paths),
+      by = (cur_path) -> lowercase(first(cur_path))
+    )
 
     return cur_paths
 end
